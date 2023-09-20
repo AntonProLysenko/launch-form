39d36bf65bf53a153ba36f2462b4dc4062893a59
boldPro <probold777@gmail.com>
2023-09-20 19:29:50 -0400

    Refactored

--- script.js
+++ script.js
@@ -10,11 +10,9 @@ let emailField = form.querySelector('input[name="email"]');
 let emailFieldError = form.querySelector("#email_error");
 
 let button = form.querySelector('input[type = "submit"]');
-
 let submissionResponse = document.querySelector("#answer");
 
 
-// console.log(LaunchFormElement);
 
 let submitHandler = (evt) => {
   evt.preventDefault();
@@ -29,17 +27,36 @@ let submitHandler = (evt) => {
 
 
 let checkInputValidity=()=>{
-    // this.style.border = "1px solid rgb(249, 72, 57)";
-    if(nameField.value.length>=1){
+ 
+    if(emailField.value.length < 5 || !emailField.value.includes("@")|| !emailField.value.includes(".")) {
+        emailField.validity.valid = false
+        emailField.setCustomValidity(" ");
+        emailFieldError.classList.remove("hidden");
+        emailField.style.border = "1px solid rgb(249, 72, 57)";
+    }else{
+        emailField.setCustomValidity("");
+        emailField.style.border = "1px solid rgb(209, 209, 209)";
+        emailFieldError.classList.add("hidden"); 
+    }
+
+    if (nameField.validity.valueMissing) {
+        nameField.setCustomValidity(" ");
+        nameFieldError.classList.remove("hidden");
+        nameField.style.border = "1px solid rgb(249, 72, 57)";
+    } else{
+        nameField.setCustomValidity("");    
         nameField.style.border = "1px solid rgb(209, 209, 209)";
         nameFieldError.classList.add("hidden");
     }
 
-    if (emailField.value.length > 5 || emailField.value.includes("@")|| emailField.value.includes(".")){
-        emailField.style.border = "1px solid rgb(209, 209, 209)";
+
+    if (emailField.validity.valid&&nameField.validity.valid){
+        nameFieldError.classList.add("hidden");
         emailFieldError.classList.add("hidden");
+        return true
     }
-    
+    emailField.reportValidity();
+    nameField.reportValidity()
 }
 
 
@@ -49,47 +66,8 @@ nameField.addEventListener("change", checkInputValidity)
 
    
 button.addEventListener("click", () => {
-
-    if(emailField.value.length < 5 || !emailField.value.includes("@")|| !emailField.value.includes(".")) {
-        emailField.validity.valid = false
-        emailField.setCustomValidity(" ");
-    }else{
-        emailField.setCustomValidity("");
-    }
-
-    if (nameField.validity.valueMissing) {
-      nameField.setCustomValidity(" ");
-      nameFieldError.classList.remove("hidden");
-      nameField.style.border = "1px solid rgb(249, 72, 57)";
-    } else{
-        nameField.setCustomValidity("");
-        nameField.style.border = " border: 1px solid rgb(49, 122, 226)"
-    }
-
-    if(!emailField.validity.valid){
-        // emailField.setCustomValidity("");
-        emailFieldError.classList.remove("hidden");
-        emailField.style.border = "1px solid rgb(249, 72, 57)";
-    }
-
-
-    if (emailField.validity.valid&&nameField.validity.valid){
-        nameFieldError.classList.add("hidden");
-        emailFieldError.classList.add("hidden");
-        form.addEventListener("submit", submitHandler);
-    }
-
-    if (emailField.validity.valid) {
-        emailFieldError.classList.add("hidden");
-    }
-
-    if(nameField.validity.valid){
-        nameFieldError.classList.add("hidden");
-    }
-
-    emailField.reportValidity();
-    nameField.reportValidity()
-
+    let valid = checkInputValidity()
+    valid && form.addEventListener("submit", submitHandler);
 })
 
 
